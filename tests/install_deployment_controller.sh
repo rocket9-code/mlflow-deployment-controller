c7f553ab48e79441a5bc67735f9cc7e1bd12d570
raghulkrishna <62284209+raghulkrishna@users.noreply.github.com>
2022-12-12 08:51:33 +0100

    feat: setup kind test (#23)

---
+++ tests/install_deployment_controller.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+set -e
+echo "Installing Deployment Controller ..."
+
+kubectl create ns mlflow
+helm install mlflow-controller charts/mlflow-controller  --set image.tag=$GITHUB_SHA
+kubectl get po -n mlflow
+
+echo "Waiting for Deployment Controller to be ready ..."
+kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (mlflow-controller)' --timeout=180s -n mlflow
