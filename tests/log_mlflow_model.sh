8f373ac53b0303ed04dc825450a95dac6c61f145
firstname lastname <youremail@email.com>
2022-12-15 18:38:37 +0530

    feat: gitops

--- tests/log_mlflow_model.sh
+++ tests/log_mlflow_model.sh
@@ -3,5 +3,7 @@ set -e
 echo "Installing Mlflow ..."
 pip install mlflow==1.25.1
 pip install protobuf==3.20.*
+pip install scikit-learn==0.23.2
+pip install pandas==0.23.4
 export MLFLOW_TRACKING_URI=http://localhost:5000
 python ./tests/mlflow/iris.py
