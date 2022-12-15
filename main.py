74ebeda8067fbcfad08b5155322be421d47a9a78
firstname lastname <youremail@email.com>
2022-12-15 21:01:03 +0530

    feat: gitops

--- main.py
+++ main.py
@@ -27,12 +27,12 @@ if __name__ == "__main__":
     # scheduler.add_job(
     #     controller.deploy_controller, CronTrigger.from_crontab("* * * * *")
     # )
-    scheduler.add_job(
-        id="controller",
-        func=controller.deploy_controller,
-        trigger="interval",
-        seconds=15,
-    )
+    # scheduler.add_job(
+    #     id="controller",
+    #     func=controller.deploy_controller,
+    #     trigger="interval",
+    #     seconds=15,
+    # )
     if os.getenv("GITOPS_ENABLED", "False"):
         scheduler.add_job(
             id="gitopsmdc",
