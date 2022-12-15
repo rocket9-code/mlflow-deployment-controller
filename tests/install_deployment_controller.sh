b99fb38424b9782e8ce629768a9cd23a5ef6021f
firstname lastname <youremail@email.com>
2022-12-15 16:37:32 +0530

    feat: setup kind test

--- tests/install_deployment_controller.sh
+++ tests/install_deployment_controller.sh
@@ -9,7 +9,8 @@ kubectl get po -n mlflow
 kubectl create secret generic github-secret --from-literal=githubtoken=testpw
 echo "Waiting for Deployment Controller to be ready ..."
 export POD_NAME=$(kubectl get pods --namespace mlflow -l "app.kubernetes.io/name=mlflow-controller" -o jsonpath="{.items[0].metadata.name}")
-kubectl describe po $POD_NAME -n mlflow
+
 # kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (mlflow-controller)' --timeout=180s -n mlflow
 sleep 180
+kubectl describe po $POD_NAME -n mlflow
 kubectl logs deployment/mlflow-controller -n mlflow
