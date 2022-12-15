479c9f320294ce10e55e9adace95d369dbfcb26e
firstname lastname <youremail@email.com>
2022-12-15 13:10:34 +0530

    feat: setup kind test

--- tests/install_mlflow.sh
+++ tests/install_mlflow.sh
@@ -6,9 +6,9 @@ helm repo add minio https://charts.bitnami.com/bitnami
 helm install minio minio/minio -n mlflow --set auth.rootUser=admin --set auth.rootPassword=admin
 
 export ROOT_USER=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-user}" | base64 -d)
-export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-password}" | base64 -d)\
-echo $ROOT_USER
+export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-password}" | base64 -d)
 
+echo $ROOT_USER
 echo $ROOT_PASSWORD
 
 kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (minio)' --timeout=180s -n mlflow
