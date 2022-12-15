d1ba823f9b4848686710cdcd2eedff0de4304a7d
firstname lastname <youremail@email.com>
2022-12-15 12:16:13 +0530

    feat: setup kind test

--- tests/install_mlflow.sh
+++ tests/install_mlflow.sh
@@ -6,7 +6,8 @@ helm repo add minio https://charts.bitnami.com/bitnami
 helm install minio minio/minio -n mlflow
 kubectl get po -n mlflow
 kubectl get sc
-kubectl get pvc , pv -n mlflow
+kubectl get pvc -n mlflow
+kubectl get  pv -n mlflow
 export ROOT_USER=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-user}" | base64 -d)
 export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-password}" | base64 -d)
 echo $ROOT_USER
