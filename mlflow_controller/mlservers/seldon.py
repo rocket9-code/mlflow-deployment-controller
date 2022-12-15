a278a97de7ae6a5043bd9d47c617d9860f347f9a
firstname lastname <youremail@email.com>
2022-12-15 22:18:17 +0530

    feat: gitops

--- mlflow_controller/mlservers/seldon.py
+++ mlflow_controller/mlservers/seldon.py
@@ -88,6 +88,7 @@ def sync(
                 model_name, _, _ = var_parser(model_jinja)
                 logger.info(model_name)
                 model = model_metadata[registry_name][backend][model_name]
+                logger.info(model)
                 run_id = model["run_id"]
                 rep_deploy_yaml = update_modeluris(
                     rep_deploy_yaml,
