d161beb45ef9b4cc5e46ded42cda4cdcba73dda7
firstname lastname <youremail@email.com>
2022-12-15 19:27:55 +0530

    feat: gitops

--- tests/log_mlflow_model.sh
+++ tests/log_mlflow_model.sh
@@ -6,5 +6,8 @@ pip install protobuf==3.20.*
 pip install scikit-learn==0.23.2
 pip install pandas==0.23.4
 pip install boto3==1.22.9
+export MLFLOW_S3_ENDPOINT_URL=http://localhost:9001
+export AWS_ACCESS_KEY_ID=minioadmin
+export AWS_SECRET_ACCESS_KEY=minioadmin
 export MLFLOW_TRACKING_URI=http://localhost:5000
 python ./tests/mlflow/iris.py
