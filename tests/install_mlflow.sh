097f9ceb80ab6e21c9e1383dd8d81af2acfa03cc
firstname lastname <youremail@email.com>
2022-12-15 13:01:55 +0530

    feat: setup kind test

--- tests/install_mlflow.sh
+++ tests/install_mlflow.sh
@@ -3,10 +3,14 @@ set -e
 echo "Installing Mlflow ..."
 kubectl create ns mlflow
 helm repo add minio https://charts.bitnami.com/bitnami
-helm install minio minio/minio -n mlflow
-helm repo add hello-mlflow https://rocket9-code.github.io/hello-mlflow/
-helm install hello-mlflow hello-mlflow/mlflow
+helm install minio minio/minio -n mlflow --set auth.rootUser=admin --set auth.rootPassword=admin
 
 export ROOT_USER=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-user}" | base64 -d)
-export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-password}" | base64 -d)
+export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-password}" | base64 -d)\
+echo $ROOT_USER
+echo $ROOT_PASSWORD
 kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (minio)' --timeout=180s -n mlflow
+
+helm repo add rocket9-code https://rocket9-code.github.io/hello-mlflow/
+helm install mlflow rocket9-code/mlflow 
+kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (mlflow)' --timeout=180s -n mlflow
