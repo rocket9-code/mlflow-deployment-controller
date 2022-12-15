e145928c8f95ad262c5d9d3cf5ebce354b77334c
firstname lastname <youremail@email.com>
2022-12-15 13:21:59 +0530

    feat: setup kind test

--- tests/install_mlflow.sh
+++ tests/install_mlflow.sh
@@ -8,9 +8,6 @@ helm install minio minio/minio -n mlflow --set auth.rootUser=admin --set auth.ro
 export ROOT_USER=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-user}" | base64 -d)
 export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="{.data.root-password}" | base64 -d)
 
-echo $ROOT_USER
-echo $ROOT_PASSWORD
-
 # kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (minio)' --timeout=180s -n mlflow
 kubectl apply -f tests/mlflow-cm.yaml -n mlflow
 helm repo add rocket9-code https://rocket9-code.github.io/hello-mlflow/
