8f2de206eeba04f71ba58b26f3da43d288033472
firstname lastname <youremail@email.com>
2022-12-15 15:59:55 +0530

    feat: setup kind test

--- tests/install_mlflow.sh
+++ tests/install_mlflow.sh
@@ -10,7 +10,7 @@ export ROOT_PASSWORD=$(kubectl get secret --namespace mlflow minio -o jsonpath="
 
 kubectl wait --for=condition=ready pod -l 'app.kubernetes.io/name in (minio)' --timeout=380s -n mlflow
 kubectl apply -f tests/mlflow-cm.yaml -n mlflow
-helm repo add rocket9-code https://rocket9-code.github.io/hello-mlflow/
+helm repo add rocket9-code https://rocket9-code.github.io/hello-mlflow
 helm install mlflow rocket9-code/mlflow  -n mlflow --set artifact.ArtifactRoot=s3://artifacts  --set envFromconfigMap=minio-cm --set image.pullPolicy=Always
 kubectl get po -n mlflow
 export POD_NAME=$(kubectl get pods --namespace mlflow -l "app.kubernetes.io/name=mlflow,app.kubernetes.io/instance=mlflow" -o jsonpath="{.items[0].metadata.name}")
