72d89aab9e0f0161b146b9d4338d857ba77065f3
firstname lastname <youremail@email.com>
2022-12-15 18:29:12 +0530

    feat: gitops

--- tests/log_mlflow_model.sh
+++ tests/log_mlflow_model.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -e
 echo "Installing Mlflow ..."
-RUN pip install mlflow==1.25.1
-RUN pip install protobuf==3.20.*
+pip install mlflow==1.25.1
+pip install protobuf==3.20.*
 export MLFLOW_TRACKING_URI=http://localhost:5000
 python mlflow/iris.py
