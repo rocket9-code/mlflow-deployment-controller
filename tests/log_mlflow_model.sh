62347fbe3943b579ae5baeb6b0f8a95d8db9c5c0
firstname lastname <youremail@email.com>
2022-12-16 13:58:25 +0530

    feat: gitops

--- tests/log_mlflow_model.sh
+++ tests/log_mlflow_model.sh
@@ -7,6 +7,7 @@ pip install scikit-learn==0.23.2
 pip install pandas==0.23.4
 pip install boto3==1.22.9
 pip install minio
+pip install kubernetes
 export MLFLOW_S3_ENDPOINT_URL=http://localhost:9000
 export AWS_ACCESS_KEY_ID=minioadmin
 export AWS_SECRET_ACCESS_KEY=minioadmin
