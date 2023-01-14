6f93e2fe5f08ad7eef0fbd67e7310832d9e97148
raghulkrishna <rrkraghulkrishna@gmail.com>
2023-01-14 12:23:55 +0100

    test kserve

--- tests/docker_build_push.sh
+++ tests/docker_build_push.sh
@@ -2,4 +2,5 @@
 set -e
 echo "Installing build test image and push ..."
 docker build -t hellomlops/mlflow-deployment-controller:$GITHUB_SHA .
-docker push hellomlops/mlflow-deployment-controller:$GITHUB_SHA
+# docker push hellomlops/mlflow-deployment-controller:$GITHUB_SHA
+kind load docker-image hellomlops/mlflow-deployment-controller:$GITHUB_SHA
