3a79cd54f9e5fc5400a190d9c75313fcf8477007
raghulkrishna <rrkraghulkrishna@gmail.com>
2023-01-14 16:54:24 +0100

    test kserve

--- tests/install_kserve_deployment_controller.sh
+++ tests/install_kserve_deployment_controller.sh
@@ -3,13 +3,14 @@ set -e
 echo "Installing Kserve Deployment Controller ..."
 kubectl create ns staging
 kubectl create ns production
+kubectl create secret generic github-secret -n mlflow --from-literal=githubtoken=password
+
 helm install mdc-staging charts/mlflow-controller  --set image.tag=$GITHUB_SHA  --set image.pullPolicy=Never --set image.repository=docker.io/hellomlops/mlflow-deployment-controller -n mlflow --set mlflow.backend=s3 --set gitops.deploymentLocation=staging/ --set mlserver=kserve 
 helm install mdc-production charts/mlflow-controller  --set image.tag=$GITHUB_SHA  --set image.pullPolicy=Never --set image.repository=docker.io/hellomlops/mlflow-deployment-controller -n mlflow --set mlflow.stage=Production --set mlflow.namespace=production --set mlflow.backend=s3 --set gitops.deploymentLocation=staging/ --set mlserver=kserve
 
 kubectl get deployment -n mlflow
 kubectl get cm -n mlflow
 kubectl get po -n mlflow
-kubectl create secret generic github-secret -n mlflow --from-literal=githubtoken=password
 echo "Waiting for Deployment Controller to be ready ..."
 export POD_NAME=$(kubectl get pods --namespace mlflow -l "app.kubernetes.io/instance=mdc-staging" -o jsonpath="{.items[0].metadata.name}")
 
