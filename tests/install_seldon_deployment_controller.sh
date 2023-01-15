a2414e8bc99ec09e1f3d0a7203c3e7ded1ffec3d
raghulkrishna <rrkraghulkrishna@gmail.com>
2023-01-15 15:23:13 +0100

    test: seldon ready

--- tests/install_seldon_deployment_controller.sh
+++ tests/install_seldon_deployment_controller.sh
@@ -29,5 +29,4 @@ export MLFLOW_TRACKING_URI=http://localhost:5000
 python ./tests/mlflow/list_model.py $mlserver
 
 
-kubectl wait --for=condition=ready seldondeployment mlflow --timeout=600s -n staging
-
+python ./tests/mlflow/test_deploy.py
