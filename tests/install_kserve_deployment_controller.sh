6785c3d488fe6d62dce809472141f3828657c96f
mdcadmin <mdcadmin@example.com>
2023-01-16 09:10:02 +0100

    test kserve

--- tests/install_kserve_deployment_controller.sh
+++ tests/install_kserve_deployment_controller.sh
@@ -4,6 +4,7 @@ echo "Installing Kserve Deployment Controller ..."
 kubectl create ns staging
 kubectl create ns production
 kubectl create secret generic github-secret -n mlflow --from-literal=githubtoken=password
+kubectl apply -f tests/repo-test/staging/kserve-sa.yaml -n staging
 
 helm install mdc-staging charts/mlflow-controller  -n mlflow  --set image.tag=$GITHUB_SHA  --set image.pullPolicy=Never  --set image.repository=docker.io/hellomlops/mlflow-deployment-controller --set mlflow.backend=s3 --set gitops.deploymentLocation=staging/ --set mlserver=kserve --set gitops.repository=gitea-http.default.svc.cluster.local:3000/mdcadmin/repo-test --set  gitops.protocol=http
 
@@ -24,3 +25,5 @@ export AWS_ACCESS_KEY_ID=minioadmin
 export AWS_SECRET_ACCESS_KEY=minioadmin
 export MLFLOW_TRACKING_URI=http://localhost:5000
 python ./tests/mlflow/list_model.py $mlserver
+
+kubectl wait --for=condition=ready pod inferenceservice sklearn-iris-minio  -n stagin  --timeout=180s
